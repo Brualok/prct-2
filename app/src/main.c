ed12f07a0e72a6b0097ee1e353c802825ea0dc73
Терземан Андрей <vasya.zalupin.77@mail.ru>
2026-07-08 21:53:22 +0500

    create main

--- app/src/main.c
+++ app/src/main.c
@@ -2,15 +2,22 @@
 main.c - главный модуль программы. 
 
 Терземан Андрей Александрович
+МК-101
 */
-
+#include <stdio.h>
+#include <stdlib.h>
 #include "lib_main.h"
 
-#include <stdlib.h>
 
+int main(int argc, char* argv[]) {
+    if (argc != 5) {
+        fprintf(stderr, "Error: exactly 4 arguments required: %s <infile> <outfile> <pattern_hex> <replacement_hex>\n", argv[0]);
+        return 1;
+    }
+    char* infile = argv[1];
+    char* outfile = argv[2];
+    char* pat_hex = argv[3];
+    char* rep_hex = argv[4];
 
-int main (int argc, char *argv[], char *envp[]) {
-
-
-	return 0;
+    return 0;
 }
