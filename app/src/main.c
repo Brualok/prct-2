635c209e04d9db4d9a746124d9e90657fb28f714
Bryalok <vasya.zalupin.77@mail.ru>
2026-07-08 20:09:48 +0500

    Добавлена структура основных файлов

---
+++ app/src/main.c
@@ -0,0 +1,16 @@
+/*
+main.c - главный модуль программы. 
+
+Терземан Андрей Александрович
+*/
+
+#include "lib_main.h"
+
+#include <stdlib.h>
+
+
+int main (int argc, char *argv[], char *envp[]) {
+
+
+	return 0;
+}
